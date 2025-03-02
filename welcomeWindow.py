2d84768a0c4dc2bdd852581020c705c632f6cf0c
neiljya <neil.j.wu@gmail.com>
2025-03-01 23:39:09 -0800

    modified frontend welcomeWindow: added logo and progress bar + nice little intro

--- welcomeWindow.py
+++ welcomeWindow.py
@@ -1,13 +1,13 @@
 
 import customtkinter
-
+from PIL import Image
 
 class WelcomeWindow(customtkinter.CTkToplevel):
     def __init__(self, parent):
         super().__init__(parent)
         self.attributes("-alpha", 0.0)
-        self.geometry("400x300")
-        self.title("Welcome to VisualLink")
+        self.geometry("400x400")
+        self.title("Welcome to VisLink")
         self.transient(parent)
         self.grab_set()
         
@@ -19,27 +19,68 @@ class WelcomeWindow(customtkinter.CTkToplevel):
         y = (height - 300) // 2
         self.geometry(f"400+300+{x}+{y}")
 
+        # Logo image
+        logo_pil = Image.open("./vislink_logo.png")
+        logo = customtkinter.CTkImage(dark_image=logo_pil, size=(200,200))
+
+        logo_label = customtkinter.CTkLabel(
+            self,
+            image=logo,
+            text=""
+        )
+
+        logo_label.pack(pady=10)
+
+
         # Welcome content
-        welcome_label = customtkinter.CTkLabel(
+        self.welcome_label = customtkinter.CTkLabel(
             self, 
             text = "Welcome to VisualLink",
             font = ("Arial", 24, "bold")
         )
-        welcome_label.pack(pady = 50)
         
-        subtitle_label = customtkinter.CTkLabel(
+        
+        self.subtitle_label = customtkinter.CTkLabel(
             self, 
             text = "Eye-tracking setup loading...",
             font = ("Arial", 14)
         )
-        subtitle_label.pack(pady = 20)
+        
+
+        # === PROGRESS BAR =====
+        self.progressbar = customtkinter.CTkProgressBar(self, width=300)
+        self.progressbar.set(0)
 
 
         # Fade in animation
         self.after(100, self.fade_in)
 
-        # Auto-close after 2 seconds
-        self.after(2000, self.close_welcome)
+        # ===== FADE IN WELCOME, LOADING, AND PROGRESS
+        self.after(1500, self.show_welcome)
+
+        self.after(2500, self.show_subtitle)
+
+        self.after(3500, self.show_progressBar)
+
+
+        # Auto-close after 12 seconds
+        self.after(1000*12, self.close_welcome)
+
+    def animate_progress(self):
+        current = self.progressbar.get()
+        if current < 1.0:
+            self.progressbar.set(current + 0.01)
+            self.after(50, self.animate_progress)
+        
+    def show_progressBar(self):
+        self.progressbar.pack(pady=10)
+        self.animate_progress()
+
+    def show_welcome(self):
+        self.welcome_label.pack(pady=10)
+    
+    def show_subtitle(self):
+        self.subtitle_label.pack(pady=20)
 
     def fade_in(self):
         alpha = self.attributes("-alpha")
