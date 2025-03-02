0decf2f4b9ff3b1f7a1f469db16692f733d1ec67
pranavputtagunta <pranav.sc30@gmail.com>
2025-03-02 00:08:24 -0800

    blink clicks

--- MouseAction.py
+++ MouseAction.py
@@ -2,15 +2,18 @@ import pyautogui
 from Rotation2Vector import Vector
 import time
 
+CLICK_LISTEN_INTERVAL = 0.2 # max interval between clicks to end listening (calibrate as needed)
+
+# One blink for left click, two blinks for right click, three blinks for double click
 class Mouse:
     def __init__(self):
         self.size = pyautogui.size()
         self.position = Vector(0, 0)
         pyautogui.FAILSAFE = True
 
-        self.first_click = False
-        self.first_click_time = time.time()
-
+        self.click_count = 0
+        self.last_click_time = time.time()
+ 
     def vector2pos(self, vector):
         return Vector((1 + vector.x) * (self.size[0] / 2), (1 - vector.y) * (self.size[1] / 2))
     
@@ -18,10 +21,29 @@ class Mouse:
         new_pos = self.vector2pos(new_vector)
         pyautogui.moveTo(new_pos.x, new_pos.y)
 
-    
+    # needs to be called in loop
+    def checkClick(self, verbose= False):
+        if self.click_count > 0 and (time.time() - self.last_click_time) > CLICK_LISTEN_INTERVAL:
+            if self.click_count == 1:
+                pyautogui.leftClick()
+                if verbose: print("left click")
+            elif self.click_count == 2:
+                pyautogui.rightClick()
+                if verbose: print("right click")
+            else:
+                pyautogui.doubleClick()
+                if verbose: print("double click")
+            self.click_count = 0
+
+    def registerClick(self):
+        self.click_count += 1
+        self.last_click_time = time.time()
 
     def left_click(self):
         pyautogui.leftClick()
 
     def right_click(self):
         pyautogui.rightClick()
+
+    def double_click(self):
+        pyautogui.doubleClick()
--- headPoseEstimator.py
+++ headPoseEstimator.py
@@ -26,7 +26,7 @@ class HeadPoseEstimator:
         self.sensitivity = SensitivityParams(sensitivity, deadzone) # set sensitivity and deadzone
         self.__init_model()
 
-    def process_img(self, img, moveMouse= True, drawMask= True, blinkAnnot= True, displayAngle= True):
+    def process_img(self, img, moveMouse= True, drawMask= True, blinkAnnot= True, displayAngle= True, verbose= False):
         mp_image = mp.Image(image_format= mp.ImageFormat.SRGB, data=cv2.flip(img, 1))
         detection_result = self.detector.detect(mp_image)
         roll, pitch, yaw = self.__get_euler_angles(detection_result)
@@ -37,8 +37,10 @@ class HeadPoseEstimator:
         display_img = mp_image.numpy_view()
         if drawMask:
             display_img = self.__draw_landmarks_on_image(display_img, detection_result)
-        if blinkAnnot:
-            display_img = self.__display_EAR(display_img, detection_result)
+        display_img, blinked = self.__detect_blink(display_img, detection_result, blinkAnnot)
+        if blinked:
+            self.mouse.registerClick()
+        self.mouse.checkClick(verbose)
         return display_img
 
     def set_sensitivity_params(self, sensitivity, deadzone):
@@ -92,46 +94,51 @@ class HeadPoseEstimator:
 
         return annotated_image
 
-    # Function to draw landmarks, lines, and EAR
-    def __display_EAR(self, rgb_image, detection_result):
+    # Function to draw landmarks, lines, and EAR, detects blinks
+    def __detect_blink(self, rgb_image, detection_result, draw_EAR):
         face_landmarks_list = detection_result.face_landmarks
         annotated_image = np.copy(rgb_image)
+        blink = False
 
         for face_landmarks in face_landmarks_list:
             h, w, _ = annotated_image.shape  # Image dimensions
 
-            for eye in [LEFT_EYE_LANDMARKS, RIGHT_EYE_LANDMARKS]:
-                # Get landmark coordinates
-                top = (int(face_landmarks[eye["top"]].x * w), int(face_landmarks[eye["top"]].y * h))
-                bottom = (int(face_landmarks[eye["bottom"]].x * w), int(face_landmarks[eye["bottom"]].y * h))
-                outer = (int(face_landmarks[eye["outer"]].x * w), int(face_landmarks[eye["outer"]].y * h))
-                inner = (int(face_landmarks[eye["inner"]].x * w), int(face_landmarks[eye["inner"]].y * h))
+            if draw_EAR:
+                for eye in [LEFT_EYE_LANDMARKS, RIGHT_EYE_LANDMARKS]:
+                    # Get landmark coordinates
+                    top = (int(face_landmarks[eye["top"]].x * w), int(face_landmarks[eye["top"]].y * h))
+                    bottom = (int(face_landmarks[eye["bottom"]].x * w), int(face_landmarks[eye["bottom"]].y * h))
+                    outer = (int(face_landmarks[eye["outer"]].x * w), int(face_landmarks[eye["outer"]].y * h))
+                    inner = (int(face_landmarks[eye["inner"]].x * w), int(face_landmarks[eye["inner"]].y * h))
 
-                # Draw vertical line (green) - between top and bottom eyelid
-                cv2.line(annotated_image, top, bottom, (0, 255, 0), 2)
+                    # Draw vertical line (green) - between top and bottom eyelid
+                    cv2.line(annotated_image, top, bottom, (0, 255, 0), 2)
 
-                # Draw horizontal line (blue) - between inner and outer eye corners
-                cv2.line(annotated_image, inner, outer, (255, 0, 0), 2)
+                    # Draw horizontal line (blue) - between inner and outer eye corners
+                    cv2.line(annotated_image, inner, outer, (255, 0, 0), 2)
 
-                # Draw eye landmarks as yellow dots
-                for point in [top, bottom, outer, inner]:
-                    cv2.circle(annotated_image, point, 4, (0, 255, 255), -1)
+                    # Draw eye landmarks as yellow dots
+                    for point in [top, bottom, outer, inner]:
+                        cv2.circle(annotated_image, point, 4, (0, 255, 255), -1)
 
             # Compute and display EAR
             left_ear = self.__calculate_EAR(face_landmarks, LEFT_EYE_LANDMARKS)
             right_ear = self.__calculate_EAR(face_landmarks, RIGHT_EYE_LANDMARKS)
             avg_ear = (left_ear + right_ear) / 2
 
-            # Display EAR on the screen
-            cv2.putText(annotated_image, f"EAR: {avg_ear:.2f}", (30, 50),
-                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+            if draw_EAR:
+                # Display EAR on the screen
+                cv2.putText(annotated_image, f"EAR: {avg_ear:.2f}", (30, 50),
+                            cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
 
-            # Blink detection message
-            if avg_ear < EAR_THRESHOLD:
+            blink = avg_ear < EAR_THRESHOLD
+
+            if blink and draw_EAR:
+                # Blink detection message
                 cv2.putText(annotated_image, "BLINK DETECTED", (30, 90),
                             cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
 
-        return annotated_image
+        return annotated_image, blink
 
     def __get_euler_angles(self, detection_result):
         matrices = detection_result.facial_transformation_matrixes
@@ -171,7 +178,7 @@ if __name__ == "__main__":
         if not success:
            break
         
-        img = tracker.process_img(img)
+        img = tracker.process_img(img, verbose= True)
 
         cv2.imshow("test", img)
         if cv2.waitKey(1) == ord('q'):
